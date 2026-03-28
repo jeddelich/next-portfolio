881987ea13fb2a634d52fa43b3a1f62141c675a2
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-28 17:56:00 -0400

    feat: add theme persist on refresh

--- contexts/ThemeContext.jsx
+++ contexts/ThemeContext.jsx
@@ -4,13 +4,21 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window === "undefined") {
+      return false;
+    }
+
+    return localStorage.getItem("theme") === "dark";
+  });
 
   useEffect(() => {
     if (darkMode) {
       document.body.classList.add("dark-theme");
+      localStorage.setItem("theme", "dark");
     } else {
       document.body.classList.remove("dark-theme");
+      localStorage.setItem("theme", "light");
     }
   }, [darkMode]);
 
