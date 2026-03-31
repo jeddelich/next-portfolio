5969eea404ffbcc14cbdbbc6a979acd61c3e45c0
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-31 11:23:29 -0400

    feat: add NFT Marketplace project

--- data/projects.ts
+++ data/projects.ts
@@ -1,6 +1,24 @@
 import { Project } from "@/types/project";
 
 const projects: Project[] = [
+    {
+    img: {
+      src: "/nft-marketplace.png",
+      alt: "NFT Marketplace",
+    },
+    title: "NFT Marketplace",
+    subtitle: "React, JavaScript, HTML, CSS",
+    description:
+      "Working on 8+ branches, my pull requests feature api integration, dynamic routes and data, and a sleek design. This project demonstrates my ability to create a responsive and user-friendly interface with carousels, countdown timers, and skeleton loading states. The marketplace allows users to not only checkout individual profiles and NFTs, but explore with a sort function, providing a seamless browsing experience. (This was a fork utilizing some basic HTML and CSS starter code provided by hannamitri on GitHub)",
+    githubLink: {
+      href: "https://github.com/jeddelich/jed-nft-marketplace",
+      text: "GitHub Repo",
+    },
+    vercelLink: {
+      href: "https://jed-internship-jed-delichs-projects.vercel.app/",
+      text: "Live Demo",
+    },
+  },
   {
     img: {
       src: "/treact-react-website-ss.png",
