56884a87b7cac148d242e75ce0d58ca6d068118a
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-01 11:49:33 -0400

    refactor: reconfigure animation keyframe to motion transition

--- components/ui/ScrollDownIcon.tsx
+++ components/ui/ScrollDownIcon.tsx
@@ -1,11 +1,20 @@
+
 import styles from "./ScrollDownIcon.module.css";
+import { motion } from "framer-motion";
 
 function ScrollDown() {
+
   return (
-    <a href="#projects" className={styles.scroll}>
+    <motion.a
+      href="#projects"
+      className={styles.scroll}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1}}
+      transition={{ duration: 0.5, delay: 6}}
+    >
       <div className={`${styles.scroll__icon} ${styles.click}`}></div>
       <div className={`fa-solid fa-arrow-down ${styles.arrow}`}></div>
-    </a>
+    </motion.a>
   );
 }
 
