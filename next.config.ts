6f02e561404011e6b10edd923db0635342336920
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-18 20:56:35 -0400

    perf: reduce Image requests by adding svgs locally

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.w3.org",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.iconscout.com",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
