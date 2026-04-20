1d63ac6242a2d60ad52f2f8d102be616b439c006
jeddelich@gmail.com <jeddelich@gmail.com>
2026-04-19 23:47:32 -0400

    perf: remove framer motion with pure css

--- components/ui/ScrollDownIcon.tsx
+++ components/ui/ScrollDownIcon.tsx
@@ -1,20 +1,16 @@
 
 import styles from "./ScrollDownIcon.module.css";
-import { motion } from "framer-motion";
 
 function ScrollDown() {
 
   return (
-    <motion.a
+    <a
       href="#projects"
       className={styles.scroll}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1}}
-      transition={{ duration: 0.5, delay: 6}}
     >
       <div className={`${styles.scroll__icon} ${styles.click}`}></div>
       <div className={`fa-solid fa-arrow-down ${styles.arrow}`}></div>
-    </motion.a>
+    </a>
   );
 }
 
